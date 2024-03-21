f63a88d9dc295db2eb3ffc153b37847cd0ac7990
emi <emilianonovelli94@gmail.com>
2024-03-21 16:56:19 -0300

    se agrega sub menu y textos de presentación y se puede seleccionar el texto de respusta.

--- lib/GUI/DialogRtaCard.dart
+++ lib/GUI/DialogRtaCard.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 class DialogRtaCard extends StatelessWidget {
-  const DialogRtaCard({super.key,required this.txt});
+  const DialogRtaCard({super.key,required this.txt, required this.callback});
   final String txt;
+  final Function(String) callback;
   @override
   Widget build(BuildContext context) {
     return Align(
@@ -11,21 +12,26 @@ class DialogRtaCard extends StatelessWidget {
         constraints: BoxConstraints(
           maxWidth: MediaQuery.of(context).size.width - 45,
         ),
-        child: Card(
-          elevation: 2,
-          color: Colors.grey[900],
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        child: GestureDetector(
+          onTap: (){
+            callback(txt);
+          },
+          child: Card(
+            elevation: 2,
+            color: Colors.grey[900],
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
 
-          margin: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
-          child: Padding(
-            padding: EdgeInsets.fromLTRB(10, 7, 10,7),
-            child: Text(
-              txt,
-              style: TextStyle(
-                fontFamily: "Courier Prime",
-                  fontSize: 16,
-              color: Colors.white),
+            margin: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(10, 7, 10,7),
+              child: Text(
+                txt,
+                style: TextStyle(
+                  fontFamily: "Courier Prime",
+                    fontSize: 16,
+                color: Colors.white),
 
+              ),
             ),
           ),
         ),
